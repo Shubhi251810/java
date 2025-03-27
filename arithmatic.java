b0be975e07269a84e92fea7b105a6ce6a3a2252f
Shubhi251810 <shubhichaudhary822@gmail.com>
2025-03-27 16:22:53 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class arithmatic
+{
+	public static void main(String[] args) 
+	{
+	    int a=10,b=3;
+	    
+		System.out.println("addition" +(a+b));
+		System.out.println("subtract" +(a-b));
+		System.out.println("multiply" +(a*b));
+		System.out.println("division" +(a/b));
+        System.out.println("mode" +(a%b));
+	}
+}
